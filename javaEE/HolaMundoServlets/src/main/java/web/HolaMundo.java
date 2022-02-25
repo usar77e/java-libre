9f267ed6742b29f9aaace0bb3bb2ea8d32d2b8bf
usar77 <agenda212013@gmail.com>
2022-02-25 17:44:46 -0300

    servlets hola mundo

---
+++ javaEE/HolaMundoServlets/src/main/java/web/HolaMundo.java
@@ -0,0 +1,17 @@
+package web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet("/holamundo")
+public class HolaMundo extends HttpServlet{
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        out.println("Hola Mundo desde servlets");
+    }
+}
